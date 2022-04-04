ec5f3c6fd985e8b64aeb5367edd22d51ac252637
Wei JiangChao <weiwus@qq.com>
2022-04-04 11:26:48 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Weibo App</title>
+</head>
+<body>
+  <h1>关于页_About</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Weibo App</title>
+</head>
+<body>
+  <h1>主页_Home</h1>
+</body>
+</html>
