22171c3c65bdf3adb3e91fa5197afda3a5a3e5aa
Wei JiangChao <weiwus@qq.com>
2022-04-06 11:25:34 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -22,7 +29,8 @@ class SessionsController extends Controller
         if (Auth::attempt($credentials, $request->has('remember'))){
             // 登录成功
             session()->flash('success','欢迎登录！');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else{
             //登录失败
             session()->flash('danger', '很抱歉，你的邮箱和密码不匹配！');
