613b62af4f0e89837d07e25a4a12021ca81b6ef4
Wei JiangChao <weiwus@qq.com>
2022-04-06 16:48:45 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,16 +26,20 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials, $request->has('remember'))){
-            // 登录成功
-            session()->flash('success','欢迎登录！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
-        } else{
-            //登录失败
-            session()->flash('danger', '很抱歉，你的邮箱和密码不匹配！');
-            return redirect()->back()->withInput();
-        }
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               $fallback = route('users.show', Auth::user());
+               return redirect()->intended($fallback);
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
 
         return;
     }
