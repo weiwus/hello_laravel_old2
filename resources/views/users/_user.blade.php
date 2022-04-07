fe9b30e4e409a199634acdccdb5829e6bf097694
Wei JiangChao <weiwus@qq.com>
2022-04-07 04:02:32 +0000

    用户列表加管理员标记

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -2,7 +2,7 @@
   <img class="me-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
   <a href="{{ route('users.show', $user) }}">
     {{ $user->id.". ".
-    $user->name.". . . . . . ".$user->email }}
+    $user->name.". . . . . . ".$user->email."____".$user->is_admin }}
   </a>
 
   @can('destroy', $user)
