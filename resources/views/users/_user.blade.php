64496e9c9147b6c0079c80140086e459c90fbdcb
Wei JiangChao <weiwus@qq.com>
2022-04-07 03:53:25 +0000

    用户列表加email信息

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -2,7 +2,7 @@
   <img class="me-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
   <a href="{{ route('users.show', $user) }}">
     {{ $user->id.". ".
-    $user->name }}
+    $user->name.". . . . . . ".$user->email }}
   </a>
 
   @can('destroy', $user)
