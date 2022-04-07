853375a53ec83eaa9d01b3ede2b2896b1d62709a
Wei JiangChao <weiwus@qq.com>
2022-04-07 16:29:04 +0000

    社交统计信息

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
 
         Model::reguard();
     }
